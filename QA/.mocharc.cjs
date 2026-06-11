8640e0894e9c3183ab8a4d372ace862d4c0725c8
jans456 <105582665+jans456@users.noreply.github.com>
2026-06-11 23:39:55 +0700

    fix(qa/reporter): add mocha-multi-reporters for terminal + allure output

--- QA/.mocharc.cjs
+++ QA/.mocharc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   spec: 'specs/**/*.spec.js',
   timeout: 30000,
-  reporter: 'allure-mocha',
-  'reporter-option': ['resultsDir=allure-results'],
+  reporter: 'mocha-multi-reporters',
+  'reporter-option': ['configFile=reporter-config.json'],
   exit: true,
 };
